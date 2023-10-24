0de3069cf3562b994a48d7575888fec2bb1021b3
Iñaki Cruz Elizondo Rios <116087814+CruzElizorios@users.noreply.github.com>
2023-10-24 05:14:29 -0300

    nuevo boton intercambiar
    se agrego la funcion intercambiar monedas, y hacer el calculo inverso

--- 01-conversor-monedas/main.js
+++ 01-conversor-monedas/main.js
@@ -8,22 +8,31 @@ let cotizacionEurArs = 1150;
 let cotizacionUSDEUR = 0.93645;
 let valor;
 
+const intercambiar = () =>{
+    let A = monedaOrigen.value
+    let B = monedaDestino.value
+    monedaOrigen.value = B
+    monedaDestino.value = A
+}
+
 const mostrarValor = () => {
     let monedasParaConversion = monedaOrigen.value + monedaDestino.value
     console.log(monedaOrigen.value)
+    console.log(monedaDestino.value)
+
     switch (monedasParaConversion) {
         case "USDARS":
             valor = valorAconvertir.value * cotizacionUsdArs
             return mostrarValorFinal.value = "$ " + valor.toFixed(2);
         case "ARSUSD":
             valor = valorAconvertir.value / cotizacionUsdArs
-            return mostrarValorFinal.value = "$ " + valor.toFixed(2);
+            return mostrarValorFinal.value = "$ " + valor.toFixed(4);
         case "EURARS":
             valor = valorAconvertir.value * cotizacionEurArs
             return mostrarValorFinal.value = "$ " + valor.toFixed(2);
         case "ARSEUR":
             valor = valorAconvertir.value / cotizacionEurArs
-            return mostrarValorFinal.value = "$ " + valor.toFixed(2);
+            return mostrarValorFinal.value = "$ " + valor.toFixed(4);
         case "USDEUR":
             valor = valorAconvertir.value * cotizacionUSDEUR
             return mostrarValorFinal.value = "$ " + valor.toFixed(2);
